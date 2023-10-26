a962ba707072658c097e460ecef3543053ae2989
NHT <71449181+huutai2312@users.noreply.github.com>
2023-10-26 09:30:01 +0700

    plugin_payment_qr

---
+++ includes/class-mc-quetma-activator.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ * @package    Mc_Quetma
+ * @subpackage Mc_Quetma/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Mc_Quetma
+ * @subpackage Mc_Quetma/includes
+ */
+class Mc_Quetma_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-mc-quetma-deactivator.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ * @package    Mc_Quetma
+ * @subpackage Mc_Quetma/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Mc_Quetma
+ * @subpackage Mc_Quetma/includes
+ */
+class Mc_Quetma_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
